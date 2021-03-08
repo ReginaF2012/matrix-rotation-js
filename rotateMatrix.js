ae0571862438aca290ba2bb72c630e9eb0b7b412
ReginaF2012 <skritschy@gmail.com>
2021-03-07 21:28:40 -0600

    double loops defined

--- rotateMatrix.js
+++ rotateMatrix.js
@@ -1,3 +1,8 @@
 function rotateNinetyDeg(squareMatrix) {
-    
+    let left = 0, right = squareMatrix.length - 1;
+    while (left < right) {
+        for (let i = 0; i < squareMatrix.length; i++)
+        left++;
+        right--;
+    }
 }
