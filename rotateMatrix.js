c45c3e183e7b9b6e9a3430e24fa23d36ec29f8e5
ReginaF2012 <skritschy@gmail.com>
2021-03-07 22:18:54 -0600

    fixed while loop

--- rotateMatrix.js
+++ rotateMatrix.js
@@ -1,11 +1,19 @@
-function rotateNinetyDeg(squareMatrix) {
-    let left = 0, right = squareMatrix.length - 1;
+function rotateNinetyDeg(matrix) {
+    let left = 0, right = matrix.length - 1;
     while (left < right) {
-        let top = left, bottom = right;
-        for (let i = 0; i < squareMatrix.length; i++) {
-
+        for (let i = left; i < right; i++) {
+            let top = left, bottom = right;
         }
         left++;
         right--;
     }
 }
+
+
+let matrix = [
+    ['A', 'B', 'C', 'D'],
+    ['E', 'F', 'G', 'H'],
+    ['I', 'J', 'K', 'L'],
+    ['M', 'N', 'O', 'P']
+            ]
+
