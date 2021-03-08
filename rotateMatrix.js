a910063cd53e6df70e4762b8182bfad4e6bc633c
ReginaF2012 <skritschy@gmail.com>
2021-03-07 22:22:05 -0600

    perform swaps, logs to test code at bottom

--- rotateMatrix.js
+++ rotateMatrix.js
@@ -3,6 +3,22 @@ function rotateNinetyDeg(matrix) {
     while (left < right) {
         for (let i = left; i < right; i++) {
             let top = left, bottom = right;
+
+            // save top left
+            let topLeft = matrix[top][left + i];
+            
+            // swap top left and bottom left
+            matrix[top][left + i] = matrix[bottom - i][left];
+
+            // swap bottom left and bottom right
+            matrix[bottom - i][left] = matrix[bottom][right - i];
+            
+            // swap bottom right and top right
+            matrix[bottom][right - i] = matrix[top + i][right];
+
+            // swap top right and top left
+            matrix[top + i][right] = topLeft;
+
         }
         left++;
         right--;
@@ -17,3 +33,8 @@ let matrix = [
     ['M', 'N', 'O', 'P']
             ]
 
+console.log('before');
+console.log(matrix);
+rotateNinetyDeg(matrix);
+console.log('after');
+console.log(matrix);
